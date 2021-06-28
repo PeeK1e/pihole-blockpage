56ca2e4595956d8650813a66d32e9532b9601fda
PeeK1e <38333220+PeeK1e@users.noreply.github.com>
2021-06-29 01:03:54 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<HTML lang="de">
+<title>BLOCKED</title>
+<head>
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+<div class="parent">
+  <div class="content">
+    <img SRC="img/logo.svg">
+    <div class="text">
+      Diese Seite ist Blockiert
+      <b>oder</b> existiert nicht.
+      <BR>
+    </div>
+  </div>
+</div>
+</body>
+</HTML>
